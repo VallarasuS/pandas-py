c59a451ffe375e0740c82a489feb38aaaac8be40
Vallarasu Sambathkumar <vals@live.in>
2026-08-19 12:13:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
-print(pd.__version__)
-
 orders = pd.read_csv(r"./data/orders.csv")
 customers = pd.read_csv(r"./data/customers.csv")
 products = pd.read_csv(r"./data/products.csv")
 
-sales =  pd.merge(orders, customers, on="customer_id").merge(products, on="product_id")
+sales =  pd.merge(
+    orders, 
+    customers, 
+    on="customer_id").merge(
+        products, 
+        on="product_id")
 
 sales["total"] = sales["price"] * sales["quantity"]
 
-print(sales.groupby("city")[["total"]].sum().sort_values("total", ascending=False).head(1))
+print(
+    sales.groupby("city")[["total"]]
+    .sum()
+    .sort_values("total", ascending=False)
+    .head(1))
+
